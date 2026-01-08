b2d675bf5f245bd2f711bacb264c65906b720d19
ghita280 <alaouighita2803@gmail.com>
2026-01-08 01:12:22 +0100

    Ajout de l'application Java avec Maven

---
+++ src/main/java/com/devops/app/Main.java
@@ -0,0 +1,11 @@
+package com.devops.app;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Bonjour c'est Ghita et bienvenue dans mon projet de DevOps !");
+    }
+    
+    public static String getMessage() {
+        return "Bonjour c'est Ghita et bienvenue dans mon projet de DevOps !";
+    }
+}
---
+++ src/test/java/com/devops/app/MainTest.java
@@ -0,0 +1,12 @@
+package com.devops.app;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class MainTest {
+    @Test
+    public void testGetMessage() {
+        String message = Main.getMessage();
+        assertEquals("Bonjour c'est Ghita et bienvenue dans mon projet de DevOps !", message);
+    }
+}
